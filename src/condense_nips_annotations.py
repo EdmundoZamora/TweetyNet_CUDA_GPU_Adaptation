c8ba39ab1bded81cfd72372262bff42478d2de74
EdmundoZamora <edzamora@ucsd.edu>
2022-04-01 13:11:32 -0700

    updte

--- src/condense_nips_annotations.py
+++ src/condense_nips_annotations.py
@@ -2,50 +2,23 @@ import numpy as np
 import os
 import pandas as pd
 import sys
-import tabulate 
-from tabulate import tabulate
 
+# list file names and take their serial number
+fnames = os.listdir(os.path.join(r"data\raw\NIPS4B_BIRD_CHALLENGE_TRAIN_TEST_WAV","temporal_annotations_nips4b"))
+csvs = [f[-7:] for f in fnames]
 
+dfs = []
+for c in csvs:
+    try:
+        # Read in each annotation file and concat to preceding dataframe.
+        df = pd.read_csv(os.path.join(r"data\raw\NIPS4B_BIRD_CHALLENGE_TRAIN_TEST_WAV","temporal_annotations_nips4b","annotation_train"+c))
+        new_df = (df.T.reset_index().T.reset_index(drop=True).set_axis([f'C{i+1}' for i in range(df.shape[1])], axis=1))
+        new_df['IN FILE'] = "nips4b_birds_trainfile"+c
+        new_df['SAMPLE RATE'] = 44100
+        dfs.append(new_df)
+    except:
+        pass
 
-# "C:\Users\lianl\Repositories\TweetyNet_CUDA_GPU_Adaptation\data\raw\NIPS4B_BIRD_CHALLENGE_TRAIN_TEST_WAV\temporal_annotations_nips4b\annotation_train001.csv"
-
-# tests
-
-# list files and print file names in list
-
-# print a dataframe
-
-# save dataframe to an out dir
-# concat to out dir
-# open the out dir df
-
-
-# lists all the files in this dir
-# for loop
-# read each csv as a pandas dataframe
-# concat each dataframe, to output dataframe in out dir
-# 
-
-
-# def find_tags(data_path, folder):
-#     fnames = os.listdir(os.path.join(data_path, "temporal_annotations_nips4b"))
-#     csvs = []
-#     for f in fnames:
-#         #print(f)
-#         csvs.append(pd.read_csv(os.path.join(data_path, "temporal_annotations_nips4b", f), index_col=False, names=["start", "duration", "tag"]))
-#         # instead of doing this we just get that info from one dataframe.! WIP
-#     # print(type(csvs[0]))
-#     # return
-#     return csvs
-
-fnames = os.listdir(os.path.join('data/raw/NIPS4B_BIRD_CHALLENGE_TRAIN_TEST_WAV', "temporal_annotations_nips4b"))
-
-# fnames = os.listdir(r"C:\Users\lianl\Repositories\TweetyNet_CUDA_GPU_Adaptation\data\raw\NIPS4B_BIRD_CHALLENGE_TRAIN_TEST_WAV\temporal_annotations_nips4b")
-csvs = []
-for f in fnames:
-    # print(f)
-    # print(f[-7:])
-    csvs.append(pd.read_csv(os.path.join(r'data\raw\NIPS4B_BIRD_CHALLENGE_TRAIN_TEST_WAV', "temporal_annotations_nips4b","annotation_train"+f[-7:])))
-
-# print(fnames)
-# print(csvs)
+nips_fix = pd.concat(dfs).reset_index(drop=True).rename(columns={'C1':'OFFSET','C2':'DURATION','C3':'MANUAL ID'})
+nips_fix.to_csv(os.path.join(r"data\out","NIPS_Annotations_condensed.csv"))
+nips_fix
